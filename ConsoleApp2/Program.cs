b06877433db60437c083fee66c143b2ec69892cf
Usuario <Usuario@DESKTOP-T1CTRGU>
2019-05-17 22:00:18 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esto llama a la funcion grados farenheit
+            int centigrados = 15;
+            int resultado = convertirafarenheit(centigrados);
+            Console.WriteLine;
+
+
+        }
+
+        //funcion a grados farenheit
+       static int convertirafarenheit(int gradoscentigrados){
+
+            int gradosfarenheit = 9 / 5 * gradoscentigrados + 32;
+            return gradosfarenheit;
+
+        }
+    }
+}
